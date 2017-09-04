a24154f66e41cd7ef202a0840ada53adaaefbf2a
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2017-09-05 02:29:30 +0530

    Create myna24.c

---
+++ myna24.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num,revnum=0,rem;
+printf("Enter an integer:\t");
+scanf("%d",&num);
+while(num!=0)
+{
+rem=num%10;
+revnum=revnum*10+rem;
+num/=10;
+}
+printf("Reversed Number = %d", revnum);
+return 0;
+}
